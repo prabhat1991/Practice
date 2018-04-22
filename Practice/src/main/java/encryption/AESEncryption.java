d89eeae867355e45434d2e7eda3f431358e87194
prabhatkumar <prabhat.choco@gmail.com>
2018-04-22 12:00:13 +0530

    AES and RSA Encryption Sample

---
+++ Practice/src/main/java/encryption/AESEncryption.java
@@ -0,0 +1,35 @@
+package encryption;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESEncryption {
+
+	public static void main(String[] args) {
+		AESEncryption app = new AESEncryption();
+		app.run();
+
+	}
+
+	public void run() {
+		try {
+			String text = "World";
+			String key = "Bar12345Bar42345"; // 128 bit key
+			// Create key and cipher
+			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			// encrypt the text
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			byte[] encrypted = cipher.doFinal(text.getBytes());
+			System.out.println("Encrypted Text: " + new String(encrypted));
+			// decrypt the text
+			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			String decrypted = new String(cipher.doFinal(encrypted));
+			System.out.println("Decrypted Text: " + decrypted);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
