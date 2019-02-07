5bfc53c7a7a58086ad6cfd7ee7883c1c213fad8a
Prabhat Kumar <prabhat.choco@gmail.com>
2019-02-07 23:40:14 +0530

    Demo commit

---
+++ Practice/src/main/java/encryption/Demo.java
@@ -0,0 +1,17 @@
+package encryption;
+
+import java.util.Hashtable;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hashtable<Integer, String> hstable = new  Hashtable<>();
+		hstable.put(0, "s");
+		hstable.put(1, "s");
+		hstable.put(0, "d");
+		System.out.println(hstable);
+	}
+
+}
+	
