9c961d4932391f9bd4911a9384c0ee3414731ccd
Prabhat Kumar <prabhat.choco@gmail.com>
2019-02-07 23:43:14 +0530

    demo commit to test jenkins in local

--- Practice/src/main/java/encryption/Demo.java
+++ Practice/src/main/java/encryption/Demo.java
@@ -1,16 +1,10 @@
 package encryption;
 
-import java.util.Hashtable;
-
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hashtable<Integer, String> hstable = new  Hashtable<>();
-		hstable.put(0, "s");
-		hstable.put(1, "s");
-		hstable.put(0, "d");
-		System.out.println(hstable);
+		System.out.println("Commit to test jenkins auo-build");
 	}
 
 }
